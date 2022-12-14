443bae768284e508b276bf7d6fcc92585e436280
yvonne0414 <yvonne9456@gmail.com>
2022-12-15 03:01:21 +0800

    add notify icon style

--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,4 +1,5 @@
-import { PostItem, Notification } from '../components';
+import { PostItem, Notification, Navbar } from '../components';
+import AuthInputs from '../components/AuthInputs';
 import Button from '../components/Button';
 
 const PostList = () => {
@@ -7,7 +8,9 @@ const PostList = () => {
       <div className="font">PostList</div>
       <PostItem />
       <Button text="跟隨" outline={true} />
-      <Notification />
+      <Notification type="warn" text="警告！！！" />
+      <Navbar haveBack={true} title="推文" />
+      <Navbar title="推文2" />
     </>
   );
 };
