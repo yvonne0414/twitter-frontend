c8d22913d62f3a24d4720d18c65b99fa89588ce6
enamelu <sasuke18tina@gmail.com>
2022-12-16 18:46:35 +0800

    Add SideBarItem : active parameter

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,15 +2,15 @@ import ACLogo from '../assets/imgs/logo.png';
 import Button from './Button';
 import SideBarItem from './SideBarItem';
 
-const SideBar = ({ title, iconName }) => {
+const SideBar = ({ title, iconName, active }) => {
   return (
-    <div className={`w-[178px] h-[1200px]`}>
+    <div className={`w-[178px] h-[1200px] flex flex-col`}>
       <img src={ACLogo} className={`w-[50px] h-[50px] mt-16`} alt="AC Logo" />
-      <SideBarItem title="首頁" iconName={'home'} />
-      <SideBarItem title="個人資料" iconName={'user'} />
-      <SideBarItem title="設定" iconName={'setting'} />
+      <SideBarItem title="首頁" iconName={'home'} activeIconName={'home_o'} active={true} />
+      <SideBarItem title="個人資料" iconName={'user'} activeIconName={'user_o'} active={false} />
+      <SideBarItem title="設定" iconName={'setting'} activeIconName={'setting_o'} active={false} />
       <Button text={'推文'} />
-      <SideBarItem title="登出" iconName={'logout'} className={`justify-items-end	`} />
+      <SideBarItem title="登出" iconName={'logout'} className={`justify-end		`} />
     </div>
   );
 };
--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -1,9 +1,8 @@
-
-const SideBarItem = ({title, onClick, iconName}) => {
+const SideBarItem = ({ title, onClick, iconName, activeIconName, active }) => {
   return (
-    <div onClick={onClick} className="flex  items-center h-[66px]	">
-      <img src={require(`../assets/imgs/icon/${iconName}.png`)} alt="" className=" w-[22.2px] h-[26px] mr-[16.9px] object-contain" />
-      <h4 className="heading-h5  ">{title}</h4>
+    <div onClick={onClick} className="flex items-center h-[66px]	">
+      <img src={require(`../assets/imgs/icon/${active ? activeIconName : iconName}.png`)} alt="" className={` w-[22.2px] h-[26px] mr-[16.9px] object-contain `} />
+      <h4 className={`heading-h5 ${active ? 'text-brand' : 'text-dark-90'}`}>{title}</h4>
     </div>
   );
 };
