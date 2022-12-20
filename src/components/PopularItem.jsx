50de5e788c4e66ee064c9dec69e9e84011e92cb5
yvonne0414 <yvonne9456@gmail.com>
2022-12-20 22:53:46 +0800

    chore PopularItem Link to ProfilePage

--- src/components/PopularItem.jsx
+++ src/components/PopularItem.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Avatar, Button } from './index';
 import { addFollow, deleteFollow } from '../apis/followship';
 import { useContext } from 'react';
@@ -7,6 +8,8 @@ import { notifyContext } from '../contexts/NotifyContext';
 const PopularItem = ({userInfo}) => {
     const [buttonText, setButtonText] = useState('跟隨')
     const [isFollowing, setIsFollowing] = useState(userInfo?.isFollowed)
+
+    const navigate = useNavigate();
 
     // 通知
     const { showNotification } = useContext(notifyContext);
@@ -42,7 +45,7 @@ const PopularItem = ({userInfo}) => {
             <Avatar style="mr-2" imgUrl={userInfo?.avatar}></Avatar>
             <div className='grow' >
                 <div className='content-l-b text-ellipsis overflow-hidden ... max-w-[83px]'>{userInfo?.name}</div>
-                <div className='content-m-r text-dark-70 text-ellipsis overflow-hidden ... max-w-[83px]'>@{userInfo?.account}</div>
+                <div className='content-m-r text-dark-70 text-ellipsis overflow-hidden ... max-w-[83px] cursor-pointer' onClick={()=>{navigate(`/profile`, {state:{userId: userInfo?.id}})}}>@{userInfo?.account}</div>
             </div>
             <Button text={buttonText} textStyle={'content-l-r whitespace-nowrap py-2 px-4'} outline={!isFollowing} onClick={handelFollow}></Button>
         </div>
