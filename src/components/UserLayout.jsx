1aa7972decee4333eaeb41ea035213db5812da09
enamelu <sasuke18tina@gmail.com>
2022-12-25 01:34:01 +0800

    PostListPage: Get more posts from server when scrolling to bottom.

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -2,32 +2,37 @@ import { PopularCollection, UserSideBar } from '../components';
 import { useEffect, useState } from 'react';
 import { getUserTop10 } from '../apis/user';
 
-const UserLayout = ({children}) => {
-    const [userTop10, setUserTop10] = useState([]);
-    const loginUserId = JSON.parse(localStorage.getItem("userInfo"))?.id ?? -1;
-  
-    useEffect(()=>{
-      async function getPopularInfoList(){
-        const top10 = await getUserTop10();
-        setUserTop10(top10)
-        // console.log('top10', top10);
-      }
-      getPopularInfoList();
-    },[])
+const UserLayout = ({ children, onScrollToBottom = null }) => {
+  const [userTop10, setUserTop10] = useState([]);
+  const loginUserId = JSON.parse(localStorage.getItem('userInfo'))?.id ?? -1;
 
-    return (
-        <div className='flex justify-between'>
-            <aside className='h-screen pt-4'>
-                <UserSideBar loginUserId={loginUserId} />
-            </aside>
-            <main  className='h-screen overflow-auto border-x border-borderC mx-6 grow'>
-                {children}
-            </main>
-            <aside className='h-screen py-4'>
-                <PopularCollection title={"推薦追蹤"} userInfoList={userTop10}/>
-            </aside>
-        </div>
-    )
+  useEffect(() => {
+    async function getPopularInfoList() {
+      const top10 = await getUserTop10();
+      setUserTop10(top10);
+      // console.log('top10', top10);
+    }
+    getPopularInfoList();
+  }, []);
+
+  function handleScroll(e) {
+    const bottom = e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight < 1;
+    if (bottom) {
+      onScrollToBottom?.();
+    }
+  }
+
+  return (
+    <div className="flex justify-between">
+      <aside className="h-screen pt-4">
+        <UserSideBar loginUserId={loginUserId} />
+      </aside>
+      <main onScroll={handleScroll} className="h-screen overflow-auto border-x border-borderC mx-6 grow">{children}</main>
+      <aside className="h-screen py-4">
+        <PopularCollection title={'推薦追蹤'} userInfoList={userTop10} />
+      </aside>
+    </div>
+  );
 };
 
 export default UserLayout;
