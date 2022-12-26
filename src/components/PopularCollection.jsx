0293ce7d832af635ae402458fc431a662adb06ce
enamelu <sasuke18tina@gmail.com>
2022-12-26 09:22:01 +0800

    PopularCollection: overflow-hidden to overflow-auto. (Otherwise some popular item will be clipped by the screen.)

--- src/components/PopularCollection.jsx
+++ src/components/PopularCollection.jsx
@@ -3,7 +3,7 @@ import { PopularItem } from './index';
 const PopularCollection = ({title, userInfoList, onFollowStateChanged}) => {
 
     return (
-        <div className='bg-dark-20 overflow-hidden rounded-2xl min-w-[273px] w-full h-full'>
+        <div className='bg-dark-20 overflow-auto rounded-2xl min-w-[273px] w-full h-full'>
             <div className='p-6 border-b border-borderC'>
                 <h4 className='heading-h4'>{title}</h4>
             </div>
